3aac91b2694d92fa6feabcb0b067ced99b88d727
Matthew McCroskey <matthew.mccroskey@gmail.com>
2017-03-18 17:47:53 -0500

    Add Mist and its Realm dependency to Example app via pod install

--- Example/Pods/Target Support Files/Pods-Mist_Example/Pods-Mist_Example-frameworks.sh
+++ Example/Pods/Target Support Files/Pods-Mist_Example/Pods-Mist_Example-frameworks.sh
@@ -85,7 +85,11 @@ strip_invalid_archs() {
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework "$BUILT_PRODUCTS_DIR/Mist/Mist.framework"
+  install_framework "$BUILT_PRODUCTS_DIR/Realm/Realm.framework"
+  install_framework "$BUILT_PRODUCTS_DIR/RealmSwift/RealmSwift.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "$BUILT_PRODUCTS_DIR/Mist/Mist.framework"
+  install_framework "$BUILT_PRODUCTS_DIR/Realm/Realm.framework"
+  install_framework "$BUILT_PRODUCTS_DIR/RealmSwift/RealmSwift.framework"
 fi
