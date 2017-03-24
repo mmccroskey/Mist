cd53ad9b47671f9fbf6ce0d818de60eaa047728a
Matthew McCroskey <matthew.mccroskey@gmail.com>
2017-03-24 17:03:11 -0500

    Update example to save Todos as well

--- Example/TinyTask (iOS)/AppDelegate.swift
+++ Example/TinyTask (iOS)/AppDelegate.swift
@@ -26,7 +26,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
             
             let titleIsGroceryList = NSPredicate(format: "title == %@", "Grocery List")
             if let savedGroceryList = publicDb.find(recordsOfType: TodoList.self, where: titleIsGroceryList).first {
+                
                 print("Here's the grocery list we saved: \(savedGroceryList)")
+                
+                let todoListIsGroceryList = NSPredicate(format: "todoList == %@", savedGroceryList)
+                let savedTodos = publicDb.find(recordsOfType: Todo.self, where: todoListIsGroceryList)
+                
+                print("Here are the items in the grocery list: \(savedTodos)")
+                
             }
             
         }
@@ -34,9 +41,24 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         let groceryList = TodoList()
         groceryList.title = "Grocery List"
         
+        let eggs = Todo()
+        eggs.title = "Eggs"
+        eggs.todoList = groceryList
+        
+        let milk = Todo()
+        milk.title = "Milk"
+        milk.todoList = groceryList
+        
+        let bread = Todo()
+        bread.title = "Bread"
+        bread.todoList = groceryList
+        
         publicDb.write {
             
             publicDb.add(groceryList)
+            publicDb.add(eggs)
+            publicDb.add(milk)
+            publicDb.add(bread)
             
         }
         
