20578cfba0f893436496eeb923b7938f45989f25
Matthew McCroskey <matthew.mccroskey@gmail.com>
2017-03-23 20:08:28 -0500

    Add PrivateDatabase and SharedDatabase to project

---
+++ Mist/Classes/PrivateDatabase.swift
@@ -0,0 +1,14 @@
+//
+//  PrivateDatabase.swift
+//  Pods
+//
+//  Created by Matthew McCroskey on 3/23/17.
+//
+//
+
+import Foundation
+
+class PrivateDatabase : NonPublicDatabase {
+
+    
+}
---
+++ Mist/Classes/SharedDatabase.swift
@@ -0,0 +1,9 @@
+//
+//  SharedDatabase.swift
+//  Pods
+//
+//  Created by Matthew McCroskey on 3/23/17.
+//
+//
+
+import Foundation
